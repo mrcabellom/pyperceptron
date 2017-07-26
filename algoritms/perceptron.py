48914bcfa4cdda94ccea3b7e219f7a48cda6fe0a
Rodrigo Cabello <mrcabello@plainconcepts.com>
2017-07-26 17:43:34 +0200

    Initial commit

---
+++ algoritms/perceptron.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Perceptron(object):
+    """Binary Perceptron"""
+
+    def __init__(self, eta=0.1, bias=0):
+        self.eta = eta
+        self.bias = bias
+        self.weights = None
+        self.errors = 0
+
+    def fit(self, x, y):
+        """Fit perceptron"""
+        self.weights = np.zeros(x.shape[1])
+        for index_x, target in zip(x, y):
+            update = self.eta * (target - self.predict(index_x))
+            self.weights += update * index_x
+            self.errors += int(update != 0.0)
+        return self
+
+    def net_input(self, x):
+        return np.dot(x, self.weights) + self.bias
+
+    def predict(self, x):
+        return np.where(self.net_input(x) > 0 , 1, 0)
---
+++ main.py
@@ -0,0 +1,20 @@
+from sklearn.cross_validation import train_test_split
+from algoritms.perceptron import Perceptron
+import pandas as pd
+
+
+def main():
+
+    dataframe = raw_data = pd.read_csv('./datasets/iris-last.csv', sep=',')
+    dataframe.replace(['Iris-setosa', 'Iris-versicolor'], [0, 1], inplace=True)
+    train, test = train_test_split(dataframe, test_size=0.5, random_state=42)
+    perceptron = Perceptron()
+    x_train = train.iloc[:, :4].values
+    y_train = train.iloc[:, -1].values
+    perceptron.fit(x_train, y_train)
+    classification = perceptron.predict(test.iloc[:, :4].values)
+    test.loc[:,'irisclassification'] = classification
+    print test
+
+if __name__ == "__main__":
+    main()
